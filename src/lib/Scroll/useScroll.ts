c7580b5037dfde12ac65ee9346f93490ed7c0f4f
Pawel Peczkowski <peczkowskip@gmail.com>
2020-03-14 12:21:40 +0100

    useScroll settings and testing

--- src/lib/Scroll/useScroll.ts
+++ src/lib/Scroll/useScroll.ts
@@ -1,33 +1,37 @@
-import React, {useLayoutEffect, useState, EventHandler} from 'react'
+import React, { useLayoutEffect, useState, RefObject } from 'react'
 import { throttle } from '../../utils/'
 
 interface IData {
-	element: {
-		current: HTMLElement | undefined
-	},
-	throttleTime: number | undefined
+	element?: RefObject<HTMLElement>
+	throttleTime?: number
 }
 
-const useScroll = (data: IData) => {
-	const [position, setPosition] = useState(null)
-	const { element, throttleTime } = data
-	const target: HTMLElement | any = element === undefined ? window : element
+const useScroll = (data?: IData) => {
+	const [position, setPosition] = useState({x: 0, y: 0})
+	const { element, throttleTime } = data || {}
 
-	useLayoutEffect(() => {
-		const handleScrollPosition = (e: React.UIEvent<HTMLElement>): void => {
-			setPosition({
-				y: e.currentTarget.scrollTop,
-				x: e.currentTarget.scrollLeft
-			})
-		}	
+	const target: HTMLElement | Window = element === undefined ? window : element.current
 	
-		const f = () => target.addEventListener('scroll', handleScrollPosition)
+	useLayoutEffect(() => {
+		const s = throttle((e: React.UIEvent): void => setPosition({
+			y: e.currentTarget.scrollTop || window.scrollY,
+			x: e.currentTarget.scrollLeft || window.scrollX
+		}), throttleTime || 0)
+	
+		const handleScrollPosition = (e: Event): void => s(e)
 		
-		throttle(f, throttleTime || 250)
-		return target.removeEventListener('scroll', handleScrollPosition)
-	},[])
+		if(target) {
+			target.addEventListener('scroll', handleScrollPosition)
+		}		
+		
+		return () => { 
+			if(target) {
+				target.removeEventListener('scroll', handleScrollPosition)
+			}
+		}
+	},[target])
 
-	return position
+	return {...position}
 }
 
 export default useScroll
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,13 @@
 import slider from './Slider/useSlider'
 import rect from './Measurement/useRect'
+import scroll from './Scroll/useScroll'
 
 export const useSlider = slider
 export const useRect = rect
+export const useScroll = scroll
 
 export default {
 	useSlider,
-	useRect
+	useRect,
+	useScroll
 }
