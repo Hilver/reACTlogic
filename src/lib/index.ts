d7e951bf21d3ef1900182d1c9d3f52d81fc7377d
Pawel Peczkowski <peczkowskip@gmail.com>
2020-03-18 16:53:07 +0100

    [#10] useInput has been created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,16 @@
 import slider from './Slider/useSlider'
 import rect from './Measurement/useRect'
 import scroll from './Scroll/useScroll'
+import input from './Input/useInput'
 
 export const useSlider = slider
 export const useRect = rect
 export const useScroll = scroll
+export const useInput = input
 
 export default {
 	useSlider,
 	useRect,
-	useScroll
+	useScroll,
+	useInput
 }
