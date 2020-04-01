9f9ca152873e3140f2f43d370e7dff3f89be9b1f
Pawel Peczkowski <peczkowskip@gmail.com>
2020-04-01 12:10:43 +0200

    [#29] bug fixed

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,11 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-rest-params */
 /* eslint-disable @typescript-eslint/no-this-alias */
 const throttle = (func: Function, limit: number): Function => {
 	let inThrottle: boolean
-	return function(args: any): void {
+	return function(): void {
+		const args = arguments
 		const context = this
 		if (!inThrottle) {
-			func.apply(context, [...args])
+			func.apply(context, args)
 			inThrottle = true
 			setTimeout(() => inThrottle = false, limit)
 		}
