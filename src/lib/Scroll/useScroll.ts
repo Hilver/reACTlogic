0d262ba7356d651aeadec57e5f8d3151e0af7675
Pawel Peczkowski <peczkowskip@gmail.com>
2020-04-03 09:10:25 +0200

    [#33]throttle time and element target covered

--- src/lib/Scroll/useScroll.ts
+++ src/lib/Scroll/useScroll.ts
@@ -25,6 +25,14 @@ const useScroll = (data?: IData): IScrollResult => {
 				throw new Error('Scrolled element should be a div, table or td!')
 			}
 		}
+		if (data.throttleTime !== undefined) {
+			if (typeof data.throttleTime !== 'number') throw new Error('ThrottleTime should be a number!')
+		}
+		if (data.targetElement !== undefined) {
+			if (data.targetElement.current !== undefined) {
+				if (data.targetElement.current.tagName === undefined) throw new Error('Target should be an HTML element!')
+			}
+		}
 	}
 	const [position, setPosition] = useState({
 		x: 0,
