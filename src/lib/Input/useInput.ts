340a27245dbe0d7d0da81a852dd19cf9e9ceda4e
Pawel Peczkowski <peczkowskip@gmail.com>
2020-04-13 14:20:37 +0200

    [#13][#56] checbkox type has been covered

--- src/lib/Input/useInput.ts
+++ src/lib/Input/useInput.ts
@@ -1,14 +1,20 @@
 import React, { useState, ChangeEvent } from 'react'
 
-type S = (string | number)
-type E = ChangeEvent<HTMLInputElement | HTMLSelectElement>
+interface IInputOptions {
+	isCheckbox: boolean;
+}
+
+type S = (string | number | boolean)
+type E = ChangeEvent<any>
 type F = (e: E) => void;
 
-const useInput = (): [S, F] => {
+const useInput = (props?: IInputOptions): [S, F] => {
 	const [value, setValue] = useState(null)
+	const { isCheckbox } = props || {}
 
 	const handleValue = (e: E): void => {
-		setValue(e.currentTarget.value)
+		const value = isCheckbox === undefined || !isCheckbox ? e.currentTarget.value : e.currentTarget.checked
+		setValue(value)
 	}
 
 	return [value, handleValue]
--- src/tests/src/lib/Input/useInput.test.tsx
+++ src/tests/src/lib/Input/useInput.test.tsx
@@ -1,28 +1,109 @@
 import React from 'react'
-import { renderHook, act } from '@testing-library/react-hooks'
 import { render, fireEvent} from '@testing-library/react'
 import { useInput } from '../../../../lib'
 
-import App from '../../../../app/App'
+interface IProps {
+	type: string;
+}
 
-const r = renderHook(() => useInput())
+const BaseInputComponent = (props: IProps): React.ReactElement => {
+	const [value, setValue] = useInput()
+
+	return (
+		<div>
+			<input data-testid='testInput' type={props.type} onChange={setValue} />
+			<div data-testid='testedDiv'>{value}</div>
+		</div>
+	)
+}
 
 describe('useInput should', () => {
-	test('returns a default values', () => {
-		const [value, setValue] = r.result.current
+	test('returns empty value as init state', () => {
+		const { getByTestId } = render(
+			<BaseInputComponent type='text'/>
+		)
 
-		expect(value).toBe(null)
+		const testingDiv = getByTestId('testedDiv')
+
+		expect(testingDiv.innerHTML).toBe('')
 	})
-	// test("returns provided value", () => {
-	// 	const {getByTestId} = render(
-	// 	 <App />
-	// 	)
-	// 	let textFromInput = getByTestId("textFromInput")
-	// 	let input = (getByTestId("textInput") as HTMLInputElement)
-	// 	input.value = "test123"
+	test('returns provided value on text type input', () => {
+		const { getByTestId } = render(
+			<BaseInputComponent type='text'/>
+		)
 
+		fireEvent.change(getByTestId('testInput'), {target: {value: 'abc'}})
 
-	// 	expect(textFromInput).toBe("test123")
+		const testingDiv = getByTestId('testedDiv')
 
-	// })
+		expect(testingDiv.innerHTML).toBe('abc')
+	})
+	test('returns provided value on number type input', () => {
+		const { getByTestId } = render(
+			<BaseInputComponent type='number'/>
+		)
+
+		fireEvent.change(getByTestId('testInput'), {target: {value: 123}})
+
+		const testingDiv = getByTestId('testedDiv')
+
+		expect(testingDiv.innerHTML).toBe('123')
+	})
+	test('returns empty value when string is passed in number type input', () => {
+		const { getByTestId } = render(
+			<BaseInputComponent type='number'/>
+		)
+
+		fireEvent.change(getByTestId('testInput'), {target: {value: 'test'}})
+
+		const testingDiv = getByTestId('testedDiv')
+
+		expect(testingDiv.innerHTML).toBe('')
+	})
+	test('returns provided value on radio button input type', () => {
+		const RadiobuttonInputComponent = (): React.ReactElement => {
+			const [value, setValue] = useInput()
+			return (
+				<div>
+					<input data-testid='radioButton1' name='radioButtonTest' type='radio' onChange={setValue} value='test1' />
+					<input data-testid='radioButton2' name='radioButtonTest' type='radio' onChange={setValue} value='test2' />
+					<div data-testid='testedDiv'>{value}</div>
+				</div>
+			)
+		}
+
+		const { getByTestId } = render(
+			<RadiobuttonInputComponent />
+		)
+
+		fireEvent.click(getByTestId('radioButton2'))
+
+		const testingDiv = getByTestId('testedDiv')
+
+		expect(testingDiv.innerHTML).toBe('test2')
+	})
+	test('returns provided value on checkbox input type', () => {
+		const CheckboxInputComponent = (): React.ReactElement => {
+			const [value, setValue] = useInput({isCheckbox: true})
+			return (
+				<div>
+					<input data-testid='checkboxInput' type='checkbox' onChange={setValue} />
+					<div data-testid='testedDiv'>{value === true ? 'checked' : 'not checked'}</div>
+				</div>
+			)
+		}
+
+		const { getByTestId } = render(
+			<CheckboxInputComponent />
+		)
+
+		const checkbox = getByTestId('checkboxInput')
+		const testingDiv = getByTestId('testedDiv')
+
+		expect(testingDiv.innerHTML).toBe('not checked')
+
+		fireEvent.click(checkbox)
+
+		expect(testingDiv.innerHTML).toBe('checked')
+	})
 })
